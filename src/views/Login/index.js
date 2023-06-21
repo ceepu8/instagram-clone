4fd61fe72b8b83acff317cdde855ebd987561ea8
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-21 22:31:49 +0700

    refactor login view

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -87,7 +87,7 @@ const PhoneScreenSlideShow = () => {
   )
 }
 
-const LoginView = () => {
+const LoginForm = () => {
   const {
     register,
     handleSubmit,
@@ -107,6 +107,93 @@ const LoginView = () => {
     console.log(data)
   }
   return (
+    <form className="w-full space-y-2" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        id="name"
+        label="Phone number, username or email"
+        register={register}
+        isHaveValue={!isEmpty(watchName)}
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        register={register}
+        isHaveValue={!isEmpty(watchPassword)}
+      />
+
+      <div>
+        <Button size="small" fullWidth type="submit" disabled={!isValid}>
+          Login
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+const LoginView = () => {
+  const renderFacebookLogin = (
+    <button
+      className="text-metallic-blue font-bold text-sm flex items-center gap-x-1 mt-8"
+      type="button"
+    >
+      <FacebookIcon width={22} height={22} />
+      Log in with Facebook
+    </button>
+  )
+
+  const renderForgotPassword = (
+    <Link href="/">
+      <p className="text-metallic-blue text-xs text-center mt-6 font-semibold">Forgot password?</p>
+    </Link>
+  )
+
+  const renderSignupNav = (
+    <div
+      className={cn(`
+            w-[350px] flex flex-col items-center justify-center p-4 mt-2
+            border border-solid border-chinese-silver
+        `)}
+    >
+      <span className="text-sm">
+        Don&apos;t have an account?
+        <Link href="/">
+          <span className="text-primary font-semibold">Sign up</span>
+        </Link>
+      </span>
+    </div>
+  )
+
+  const renderAppDownloadNav = (
+    <div className="flex flex-col items-center mt-4 space-y-4">
+      <p className="text-sm">Get the app.</p>
+      <div className="flex gap-x-2">
+        <Image width={140} height={80} src="/app-store.png" alt="Download App Store" />
+        <Image
+          width={140}
+          height={80}
+          src="/google-play-store.png"
+          alt="Download Google Play Store"
+        />
+      </div>
+    </div>
+  )
+
+  const Or = (
+    <div className="mt-4 w-full">
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-t border-solid border-gainsboro" />
+        </div>
+        <div className="relative flex justify-center text-sm">
+          <span className="bg-white px-3 text-nickle font-semibold">OR</span>
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="hidden md:block relative shrink-0">
         <Image width={480} height={640} src="/phones.png" alt="Phone" />
@@ -114,97 +201,19 @@ const LoginView = () => {
       </div>
       <div>
         <div
-          className={cn(`
+          className="
             w-[350px] flex flex-col items-center justify-center p-10
-            border border-solid border-chinese-silver
-        `)}
+            border border-solid border-chinese-silver"
         >
-          <div className="mb-8">
-            <InstagramLetterIcon width={180} height="auto" />
-          </div>
-          <form className="w-full space-y-2" onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              id="name"
-              label="Phone number, username or email"
-              register={register}
-              isHaveValue={!isEmpty(watchName)}
-            />
-
-            <Input
-              id="password"
-              label="Password"
-              type="password"
-              register={register}
-              isHaveValue={!isEmpty(watchPassword)}
-            />
-
-            <div>
-              <Button size="small" fullWidth type="submit" disabled={!isValid}>
-                Login
-              </Button>
-            </div>
-          </form>
-
-          <div className="mt-4 w-full">
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-solid border-gainsboro" />
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="bg-white px-3 text-nickle font-semibold">OR</span>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <button
-              className={cn(
-                `
-                    text-metallic-blue font-bold text-sm
-                    flex items-center gap-x-1
-                    mt-8
-                `
-              )}
-              type="button"
-            >
-              <FacebookIcon width={22} height={22} />
-              Log in with Facebook
-            </button>
-
-            <Link href="/">
-              <p className="text-metallic-blue text-xs text-center mt-6 font-semibold">
-                Forgot password?
-              </p>
-            </Link>
-          </div>
+          <InstagramLetterIcon width={180} height="auto" className="mb-8" />
+          <LoginForm />
+          {Or}
+          {renderFacebookLogin}
+          {renderForgotPassword}
         </div>
 
-        <div
-          className={cn(`
-            w-[350px] flex flex-col items-center justify-center p-4 mt-2
-            border border-solid border-chinese-silver
-        `)}
-        >
-          <span className="text-sm">
-            Don&apos;t have an account?{' '}
-            <Link href="/">
-              <span className="text-primary font-semibold">Sign up</span>
-            </Link>
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center mt-4 space-y-4">
-          <p className="text-sm">Get the app.</p>
-          <div className="flex gap-x-2">
-            <Image width={140} height={80} src="/app-store.png" alt="Download App Store" />
-            <Image
-              width={140}
-              height={80}
-              src="/google-play-store.png"
-              alt="Download Google Play Store"
-            />
-          </div>
-        </div>
+        {renderSignupNav}
+        {renderAppDownloadNav}
       </div>
     </div>
   )
