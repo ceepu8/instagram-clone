8556ef101290fa872933f5b876ba96dcefb10ce1
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-22 01:48:08 +0700

    move login into authviews

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,16 +1,13 @@
 import Head from 'next/head'
 
-import LoginView from '@/views/AuthViews/Login'
-
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <>
       <Head>
         <title>Instagram</title>
       </Head>
-      <LoginView />
     </>
   )
 }
 
-export default LoginPage
+export default RegisterPage
