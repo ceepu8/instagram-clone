e77df77ad944b401578b43dfbd9b911e3e4f5f67
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-25 11:59:32 +0700

    user layout authorize

--- src/layouts/UserLayout/index.js
+++ src/layouts/UserLayout/index.js
@@ -1,8 +1,19 @@
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
+import { useRouter } from 'next/navigation'
+import { useMemo } from 'react'
 
 import AppSideBar from './AppSideBar'
 
 const UserLayout = ({ children }) => {
+  const session = useSession()
+  const router = useRouter()
+
+  const isAuthenticated = useMemo(() => session?.status === 'authenticated', [session?.status])
+
+  if (!isAuthenticated) {
+    router.push('/')
+  }
   return (
     <>
       <Head>
