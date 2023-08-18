724136702a8a0c0abd9823c0f90b1d4a092aec97
ceepu8 <caophuonguyen132002@gmail.com>
2023-08-18 23:37:42 +0700

    feat(home story): add basic slider

--- src/views/HomeView/StorySlider/index.js
+++ src/views/HomeView/StorySlider/index.js
@@ -1,9 +1,71 @@
 import { Pressable } from '@react-aria/interactions'
 import Image from 'next/image'
+import { Slide } from 'react-slideshow-image'
 
+import { ChevronLeft, ChevronRight } from '@/components/icons'
 import Assets from '@/constants/Assets'
+import { cn } from '@/utils'
 
-const StoryItem = ({ user, active = true }) => {
+const Slideshow = ({
+  children,
+  show: Show = Slide,
+  duration = 2000,
+  transitionDuration = 1000,
+  defaultIndex = 0,
+  indicators = false,
+  autoplay = true,
+  infinite = true,
+  pauseOnHover = true,
+  canSwipe = false,
+  arrows = true,
+  cssClass = '',
+  rootClass = '',
+  slidesToScroll = 2,
+  slidesToShow = 2,
+  onChange = () => {},
+  onStartChange = () => {},
+  width,
+  arrowClassname,
+}) => {
+  const nextArrow = (
+    <button className={cn(arrowClassname)} type="button">
+      <ChevronRight />
+    </button>
+  )
+  const prevArrow = (
+    <button className={cn(arrowClassname)} type="button">
+      <ChevronLeft />
+    </button>
+  )
+
+  const slideProps = {
+    indicators,
+    duration,
+    transitionDuration,
+    defaultIndex,
+    prevArrow,
+    nextArrow,
+    arrows,
+    autoplay,
+    infinite,
+    pauseOnHover,
+    canSwipe,
+    easing: 'linear',
+    cssClass,
+    slidesToScroll,
+    slidesToShow,
+    onStartChange,
+    onChange,
+  }
+
+  return (
+    <div style={{ maxWidth: width }} className={cn('w-full', rootClass)}>
+      <Show {...slideProps}>{children}</Show>
+    </div>
+  )
+}
+
+const StoryItem = ({ index, user, active = true }) => {
   const activeStyle = active
     ? { background: 'linear-gradient(45deg, #ffd600, #ff7a00, #ff0069, #d300c5, #7638fa)' }
     : { background: 'var(--chinese-silver)' }
@@ -23,6 +85,7 @@ const StoryItem = ({ user, active = true }) => {
           </div>
         </div>
         <p className="truncate text-xs">{user?.username || 'username123123'}</p>
+        <span>{index}</span>
       </div>
     </Pressable>
   )
@@ -30,13 +93,24 @@ const StoryItem = ({ user, active = true }) => {
 
 const StorySlider = () => {
   return (
-    <div className="flex space-x-2">
+    <Slideshow
+      transitionDuration={300}
+      autoplay={false}
+      slidesToShow={8}
+      slidesToScroll={8}
+      rootClass="mx-auto"
+      width={680}
+      infinite={false}
+      arrowClassname="top-6 bg-popover rounded-full border-philippine-gray border-[0.25px]"
+    >
       {Array(8)
         .fill('')
-        .map((index) => (
-          <StoryItem key={index} />
+        .map((_) => (
+          <div key={_} className="flex justify-center">
+            <StoryItem alt="show-image" />
+          </div>
         ))}
-    </div>
+    </Slideshow>
   )
 }
 
--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -4,8 +4,10 @@ import StorySlider from './StorySlider'
 const HomeView = () => {
   return (
     <div className="flex justify-center gap-16">
-      <div className="flex flex-1 flex-col items-center space-y-12 pt-12">
-        <StorySlider />
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <div className="relative h-[100px] w-full pt-12">
+          <StorySlider />
+        </div>
         <PostList />
       </div>
       <div className="hidden w-full max-w-[var(--feed-sidebar-width)] lg:block">Feed Sidebar</div>
