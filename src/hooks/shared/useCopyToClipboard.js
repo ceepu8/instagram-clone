b2ade3bfdd92fc1a0c91d221d1e08d65ffcc8137
tcdtist <chithanh.tist@gmail.com>
2023-08-24 09:53:16 +0700

    chore(shared-hooks): add useCopyToClipboard

---
+++ src/hooks/shared/useCopyToClipboard.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+import delay from 'lodash/delay'
+import { useState } from 'react'
+
+export function useCopyToClipboard() {
+  const [copiedText, setCopiedText] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const copyToClipboard = async (text) => {
+    if (!navigator?.clipboard) {
+      console.warn('Clipboard not supported')
+      return false
+    }
+
+    setIsLoading(true)
+
+    try {
+      await navigator.clipboard.writeText(text)
+      setCopiedText(text)
+
+      // delay 1 second
+      await new Promise((resolve) => {
+        delay(() => {
+          resolve()
+        }, 1000)
+      })
+
+      return true
+    } catch (error) {
+      console.warn('Copy failed', error)
+      setCopiedText(null)
+      return false
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { copiedText, copyToClipboard, isLoading }
+}
