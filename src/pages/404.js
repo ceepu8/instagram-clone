92eb512bedb42f4f9a0db7a25c1654279d33242d
tcdtist <chithanh.tist@gmail.com>
2023-09-04 16:29:51 +0700

    build: production

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,9 @@
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+const NotFoundPage = () => {
+  return (
+    <section>
+      <h1>404 - Page Not Found</h1>
+    </section>
+  )
 }
+
+export default NotFoundPage
